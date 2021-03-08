e34acb342630e9c78794c5f2966f4612502af265
Artur Hamernik <hamernik18@gmail.com>
2021-03-08 09:29:01 +0100

    navbar

---
+++ navbar.js
@@ -0,0 +1,9 @@
+let nav = '<nav>' +
+    '<ul>' +
+    '<li><a href="index.html">Home</a></li>' +
+    '<li><a href="contact.html">Contact</a></li>' +
+    '</ul>' +
+    '</nav>';
+
+let elem = document.querySelector(".navbar");
+elem.innerHTML = nav;
